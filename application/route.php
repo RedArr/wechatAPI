6a981d8896a78d6ffb5adb2322b763d77b4ee407
RedArr <miao8627@qq.com>
2018-03-12 14:49:16 +0800

    下单事务处理

---
+++ application/route.php
@@ -0,0 +1,36 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+use think\Route;
+
+Route::get('api/:version/banner/:id', 'api/:version.Banner/getBanner');
+
+Route::get('api/:version/theme', 'api/:version.Theme/getSimpleList');
+
+Route::get('api/:version/theme/:id','api/:version.Theme/getComplexOne');
+
+//Route::get('api/:version/product/recent','api/:version.Product/getRecent');
+//Route::get('api/:version/product/by_category','api/:version.Product/getAllIncategory');
+//Route::get('api/:version/product/:id','api/:version.product/getOne',[],['id'=>'\d+']);
+Route::group('api/:version/product',function (){
+    Route::get('by_category','api/:version.Product/getAllIncategory');
+    Route::get('recent','api/:version.Product/getRecent');
+    Route::get(':id','api/:version.product/getOne',[],['id'=>'\d+']);
+});
+Route::get('api/:version/category/all','api/:version.Category/getAllCategory');
+
+Route::post('api/:version/token/user', 'api/:version.Token/getToken');
+
+Route::post('api/:version/address', 'api/:version.Address/createOrUpdateAddress');
+//测试
+Route::post('api/:version/test', 'api/:version.Address/test');
+
+Route::post('api/:version/order','api/:version.Order/placeOrder');
